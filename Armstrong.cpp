42c9528bd2779b86bd62bfabe5fe31cb0470e338
LakshyaWalia09 <lakshyawalia09@gmail.com>
2025-08-21 01:43:03 +0530

    fix

--- Armstrong.cpp
+++ Armstrong.cpp
@@ -1,14 +1,17 @@
 // User function Template for C++
-class Solution {
-  public:
-    bool armstrongNumber(int n) {
-        int temp=0;
-        int m=n;
+class Solution
+{
+public:
+    bool armstrongNumber(int n)
+    {
+        int temp = 0;
+        int m = n;
         // code here
-        while(m){
-            temp+=pow((m%10),3);
-            m/=10;
+        while (m)
+        {
+            temp += pow((m % 10), 3);
+            m /= 10;
         }
-        return (temp==n)?true:false;
+        return (temp == n) ? true : false;
     }
 };
